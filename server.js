0e7ca513b85e48b3e7cd8222db08a33a5af25635
MathewG2099 <160433914+MathewG2099@users.noreply.github.com>
2024-05-30 23:40:52 -0400

    3.0
    Added git ignore

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ const app = express();
 // Express middleware will always run the operation in the order from top to bottom "order matters"
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
-app.use(html_routes)
+app.use(express.static("Public"));
 app.use(api_routes)
-
+app.use(html_routes)
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
